13f0b54e0bf8db78c6760766a2b9236942ab94d2
Aditya Saputra <aditsputra.cv@gmail.com>
2026-03-07 17:06:57 +0700

    feat: implement event handling for game start, interrogation, and menu navigation

---
+++ src/ui/event.js
@@ -0,0 +1,53 @@
+import { startGame, initGame, resetGameState } from "../main.js";
+import { pickRandomCase } from "../utils/randomCases.js";
+import {
+  showScene,
+  sceneLoading,
+  sceneStart,
+  sceneBook,
+  sceneInterrogation,
+} from "../utils/showScene.js";
+import { renderCaseBook } from "./renderBook.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const startGameBtn = document.getElementById("btn-start-game");
+  const backToStartBtn = document.getElementById("btn-back-to-start");
+  const startInterrogationBtn = document.getElementById("startInterrogation");
+
+  // Loading screen
+  showScene(sceneLoading);
+
+  setTimeout(() => {
+    showScene(sceneStart);
+  }, 1000);
+
+  // Start game
+  if (startGameBtn) {
+    startGameBtn.addEventListener("click", () => {
+      resetGameState();
+
+      const caseData = pickRandomCase();
+
+      initGame(caseData);
+      renderCaseBook(caseData);
+
+      showScene(sceneBook);
+    });
+  }
+
+  // Start interrogation
+  if (startInterrogationBtn) {
+    startInterrogationBtn.addEventListener("click", () => {
+      showScene(sceneInterrogation);
+      startGame();
+    });
+  }
+
+  // Back to menu
+  if (backToStartBtn) {
+    backToStartBtn.addEventListener("click", () => {
+      resetGameState();
+      showScene(sceneStart);
+    });
+  }
+});
